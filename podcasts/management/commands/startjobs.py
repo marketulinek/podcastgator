f56cbae94d47fbea934d1471f66540c6308fbed4
Marketa <habranova.marketa@gmail.com>
2022-08-18 22:10:16 +0200

    Upgraded code to be more reusable

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -4,25 +4,39 @@ from dateutil import parser
 from podcasts.models import Episode
 
 
+def save_new_episodes(feed):
+
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image['href']
+
+    for item in feed.entries:
+
+        if not Episode.objects.filter(guid=item.guid).exists():
+
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid
+            )
+            episode.save()
+
+def fetch_realpython_episodes():
+    feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episodes(feed)
+
+
+def fetch_talkpython_episodes():
+
+    feed = feedparser.parse("https://talkpython.fm/episodes/rss")
+    save_new_episodes(feed)
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         
-        feed = feedparser.parse('https://realpython.com/podcasts/rpp/feed')
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image['href']
-
-        for item in feed.entries:
-
-            if not Episode.objects.filter(guid=item.guid).exists():
-
-                episode = Episode(
-                    title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid
-                )
-                episode.save()
+        fetch_realpython_episodes()
+        fetch_talkpython_episodes()
